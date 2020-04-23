840725ebc0d1f23599f699221333a88765cd5f91
libanesd <44514484+libanesd@users.noreply.github.com>
2020-04-23 11:53:41 -0300

    Create TwoThreadTest.java

---
+++ TwoThreadTest.java
@@ -0,0 +1,8 @@
+package com.unitins;
+
+public class TwoThreadsTest {
+    public static void main (String[] args) {
+        new SimpleThread("Thread 1").start();
+        new SimpleThread("Thread 2").start();
+    }
+}
